55f7400b3bcf6ed2fc0d3a6341517cf36646d5c3
ckamalakkannan <37569698+ckamalakkannan@users.noreply.github.com>
2020-12-20 12:59:44 +0530

    Update ec2-terraform.tf

--- ec2-terraform.tf
+++ ec2-terraform.tf
@@ -27,6 +27,12 @@ resource "aws_instance" "name" {
 
 }
 
-resource "aws_security_group" "server-sg" {
-    name        = "server-sg"
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
